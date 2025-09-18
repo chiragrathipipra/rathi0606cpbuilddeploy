f4130acbf514681682feace62aedd315cdc6b399
chiragrathipipra <chirag@pipra.solutions>
2025-09-18 21:19:02 +0530

    second deployment

--- ec2-userdata.md
+++ ec2-userdata.md
@@ -20,7 +20,7 @@ fi
 
 # Install CodeDeploy agent
 cd /home/ec2-user
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-ap-northeast-1.s3.ap-northeast-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo systemctl enable codedeploy-agent
--- src/main/java/com/HelloController.java
+++ src/main/java/com/HelloController.java
@@ -1,17 +1,30 @@
+
 package com;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-	
 	@GetMapping("/welcome")
-	public String welcome()
-	{
+	public String welcome() {
 		return "Code is deployed in Code Deploy Again";
 	}
 
+	// Endpoint to serve the map page
+	@GetMapping(value = "/state/{stateName}", produces = MediaType.TEXT_HTML_VALUE)
+	public void getStateMap(@PathVariable String stateName, HttpServletResponse response) throws IOException {
+		// Serve the static HTML file
+		ClassPathResource htmlFile = new ClassPathResource("static/india_map.html");
+		response.setContentType(MediaType.TEXT_HTML_VALUE);
+		StreamUtils.copy(htmlFile.getInputStream(), response.getOutputStream());
+	}
 }
